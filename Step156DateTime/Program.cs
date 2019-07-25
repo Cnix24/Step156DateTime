05d42e74396547a746db860d09798f8c6a05aa87
Cnix24 <calvinattl24@gmail.com>
2019-07-25 14:55:13 -0600

    Update

---
+++ Step156DateTime/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Step156DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please provide a whole number: ");
+            string userInput = Console.ReadLine();
+            int userInt = Convert.ToInt32(userInput);
+
+            //get current time
+            DateTime nowTime = DateTime.Now;
+
+            TimeSpan xHours = new TimeSpan(userInt, 0, 0);
+
+
+
+            DateTime timeLater = nowTime + xHours;
+            Console.WriteLine("It will be "+timeLater+" in "+userInt+" hours. The number you provided.");
+        }
+    }
+}
